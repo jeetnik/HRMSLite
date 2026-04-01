7517eacfdeed9a92dfd88d07de789182967819c5
saijeetnik <saijeet01@gmail.com>
2026-04-01 11:57:50 +0530

    feat:init the db packgae

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello via Bun!");
+import { PrismaClient } from "./generated/prisma/client";
+
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+
+export const prisma = globalForPrisma.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
+
+export { PrismaClient };
+export type { Employee, Attendance } from "./generated/prisma/client";
